ab4d41b2935b0c6782190c88fb3d32d6b968ab08
PunitVJain <putwind@gmail.com>
2021-11-09 23:25:09 +0530

    trying to understand requests

--- requests_lib/first.py
+++ requests_lib/first.py
@@ -5,6 +5,6 @@ url = "https://www.google.com/"
 
 response = requests.get(url)
 
-#print(response.url)
-#print(response.headers)
-print(response.text)
+print(response.url)
+print(response.headers['Date'])
+#print(response.text)
