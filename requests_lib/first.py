55ad271f75038b4b7a1bc5c6712603368eb775e9
PunitVJain <putwind@gmail.com>
2021-11-09 23:34:34 +0530

    get method in requests

--- requests_lib/first.py
+++ requests_lib/first.py
@@ -2,9 +2,12 @@ import requests
 
 url = "https://www.google.com/"
 
-
+#  GET simple method 
 response = requests.get(url)
 
-print(response.url)
-print(response.headers['Date'])
+#print(response.url)
+#print(response.headers['Date'])
 #print(response.text)
+print(dir(response))
+
+
