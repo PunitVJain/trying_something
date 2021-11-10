5d7854e63197eabe32f633a2749e68a68089d251
PunitVJain <putwind@gmail.com>
2021-11-10 23:42:32 +0530

    post method

--- requests_lib/first.py
+++ requests_lib/first.py
@@ -8,6 +8,12 @@ response = requests.get(url)
 #print(response.url)
 #print(response.headers['Date'])
 #print(response.text)
-print(dir(response))
+#print(dir(response))
+
+# POST method
+res = requests.post(url)
+#print(res.url)
+#print(res.headers)
+print(res.text)
 
 
