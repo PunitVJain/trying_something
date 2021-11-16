f11c7c2a9e108a359f827f5460de41354fa8f39f
PunitVJain <putwind@gmail.com>
2021-11-16 14:57:31 +0530

    second REST API

--- flask_complete/first.py
+++ flask_complete/first.py
@@ -12,9 +12,15 @@ class FirstAPI(Resource):
     
     def post(self):
         return({"first": "Start with REST"})
-    
+
+class SecondAPI(Resource):
+
+    def get(self):
+        return "Second, API with flask REST"
+
 
 api.add_resource(FirstAPI, "/api/helloworld")
+api.add_resource(SecondAPI, "/api/secondapi")
 
 if __name__ == "__main__":
     app.run(debug=True, port= 3300)
