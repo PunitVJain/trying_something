0eae0e4fdd730f22c90969200e558a3323f4cbfa
PunitVJain <putwind@gmail.com>
2021-11-27 22:40:32 +0530

    time

--- datatime_lib/first.py
+++ datatime_lib/first.py
@@ -1,7 +1,9 @@
 import datetime as dt
+import time
 
 current_time = dt.datetime.now() #  current time stamp
 print(current_time.strftime('%d-%m-%Y'))
 print(current_time.today().strftime('%d-%m-%Y')) # todays date.
-
+#print(time())
+print(time.localtime())
 
